93e6ed592ede4e6cbe7bbd929b1004a8f7dcfce9
_3tHeR_ <3ther.not.found@gmail.com>
2025-11-10 13:10:49 +0100

    something random, no glue what these changes are

--- src/game.rs
+++ src/game.rs
@@ -1,3 +1,128 @@
-use crate::character::*;
-struct Game {
+pub mod character;
+pub mod map;
+pub mod physic;
+mod networking;
+
+use serde::{Serialize, Deserialize};
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
+
+use std::collections::HashMap;
+use std::thread;
+use crate::game::networking::*;
+use uuid::Uuid;
+use crate::game::{
+    character::Character,
+    map::*,
+
+};
+use std::{
+    fs,
+    io::{BufReader, prelude::*},
+    net::{TcpListener, TcpStream},
+};
+
+pub struct Player {
+    id: Uuid,
+    last_ping: usize,
+    name: String,
+    instance: Option<usize>,   
+}
+impl Player {
+    pub fn new(name: String) -> Player {
+        Player {
+            id: Uuid::new_v4(),
+            last_ping: 0,
+            name,
+            instance: None,
+        }
+    }
+}
+pub struct Game {
+    password: String,
+    addres: String,
+
+    characters: HashMap<u32,Character>,
+    players: Arc<Mutex<Vec<Player>>>,
+    map: Arc<Mutex<Option<Map>>>,
+}
+impl Game {
+    pub fn default() -> Game {
+        Game {
+            password: String::from(""),
+            addres: String::from("127.0.0.1:3621"),
+            // characters: Character::load_all(),
+            characters: HashMap::new(),
+            players: Arc::new(Vec::new().into()),
+            // map: Arc::new(None.into()),
+            map: Arc::new(Some(Map::test()).into()),
+        }
+    }
+    pub fn new(password: String,addres: String) -> Game {
+        Game {
+            password,
+            addres,
+            characters: Character::load_all(),
+            players: Arc::new(Vec::new().into()),
+            map: Arc::new(None.into()),
+        }
+    }
+    pub fn start(&mut self) {
+        let listener = TcpListener::bind(self.addres.clone())
+            .expect("Binding addres was unsucesfull");
+
+        let (map_pointer,player_pointer) = (Arc::clone(&self.map),Arc::clone(&self.players));
+
+        let network = thread::spawn(move ||
+            for stream_er in listener.incoming() {
+                let stream = stream_er.unwrap();
+                Self::handle_connection(stream,&map_pointer,&player_pointer);
+            }
+        );
+        println!("Threded");
+
+        loop {
+            thread::sleep(Duration::from_millis(1000));
+            loop {
+                if let Ok(ref mut map_opt) = self.map.try_lock() &&
+                    let Some(map) = &mut **map_opt {
+                        map.counter += 1;
+                        break;
+
+                }
+            }
+        }
+        unreachable!();
+    }
+    fn handle_connection(mut stream: TcpStream,map_ref: &Arc<Mutex<Option<Map>>>,players_ref: &Arc<Mutex<Vec<Player>>>) {
+        let buf_reader = BufReader::new(&stream);
+        let binding = buf_reader.lines().next().unwrap().unwrap();
+        let rq_line: Vec<&str> = binding.split_whitespace().collect();
+
+        dbg!(&rq_line);
+        const GET: &&str = &"GET";
+        let response = 
+            if let (Some(one),Some(two)) = (rq_line.get(0),rq_line.get(1)) {
+                match (one,two) {
+                    (GET,&"/") => Response::new(ResponseStatus::Ok,BodyType::HTML, "<head><meta http-equiv=\"refresh\" content=\"0; url=https://github.com/3ther-joyboy/Nebula\" />"),
+                    (GET,&"/map/") => {
+                        loop {
+                            if let Ok(map) = map_ref.try_lock() {
+                                if let Some(out) = (*map).clone() {
+                                    break Response::new(ResponseStatus::Ok,BodyType::JSON, &out.to_string());
+                                } else {
+                                    break Response::new(ResponseStatus::Ok,BodyType::JSON,"{}");
+                                }
+                            }
+                        }
+                    },
+                    // (&"CONNECT",&"/map/") => {},
+                    (_,_) => Response::new(ResponseStatus::None,BodyType::JSON,""),
+                }
+            } else {
+                Response::new(ResponseStatus::Error("REQUEST OR PATH NOT FOUND".to_string()),BodyType::JSON,"")
+            };
+
+        stream.write_all(response.to_string().as_bytes()).unwrap();
+    }
 }
